b1be700eec3da0bc672f93349b199b5729ee7732
Paul Tomchik <pauljtomchik@gmail.com>
2015-10-10 20:07:16 -0400

    Fixed merge issue.

--- src/services/ConverterService.js
+++ src/services/ConverterService.js
@@ -8,10 +8,10 @@ var ConverterStream  = require('MTA_Subway_GTFS-Realtime_to_SIRI_Converter').Con
     ConfigService    = require('./ConfigsService') ,
     converterConfig  = ConfigService.getConverterConfig() ,
 
+
     converterStream  = new ConverterStream(gtfsFeed, gtfsrtFeed, converterConfig, updateConverter);
 
 
-//TODO: Converter stream needs an updateconfig method.
 
 var latestConverter = null;
 
@@ -22,6 +22,15 @@ ConfigService.addConverterConfigUpdateListener(converterStream.updateConfig);
 function updateConverter (converterUpdate) {
     latestConverter = converterUpdate;
 }
+
+
+//function getStopMonitoringResponse (params, resp, callback) {
+    //latestConverter.getStopMonitoringResponse(params, resp, callback);
+//}
+
+//function getVehicleMonitoringResponse (params, resp, callback) {
+    //latestConverter.getVehicleMonitoringResponse(params, resp, callback);
+//}
 
 function getStopMonitoringResponse (params) {
     var smr = latestConverter.getStopMonitoringResponse(params);
@@ -34,14 +43,21 @@ function getStopMonitoringResponse (params) {
 function getVehicleMonitoringResponse (params) {
     var smr = latestConverter.getVehicleMonitoringResponse(params);
 
-    smr.timestamper.stamp(); //FIXME
+    smr.timestamper.stamp(); 
     return smr.response;
+}
+
+
+
+function getCurrentGTFSRealtimeTimestamp () {
+    return latestConverter.getCurrentGTFSRealtimeTimestamp();
 }
 
 
 converterStream.start();
 
 module.exports = {
-    getStopMonitoringResponse    : getStopMonitoringResponse    ,
-    getVehicleMonitoringResponse : getVehicleMonitoringResponse ,
+    getStopMonitoringResponse       : getStopMonitoringResponse       ,
+    getVehicleMonitoringResponse    : getVehicleMonitoringResponse    ,
+    getCurrentGTFSRealtimeTimestamp : getCurrentGTFSRealtimeTimestamp ,
 };
